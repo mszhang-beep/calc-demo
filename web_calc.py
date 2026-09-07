54f87052dac594df84b2cf431ac5cb013a116e3e
mszhang-beep <2064412336@qq.com>
2026-09-08 00:39:13 +0800

    增加底部署名头像

--- web_calc.py
+++ web_calc.py
@@ -15,7 +15,7 @@ st.divider()
 avatar_url = "https://cdn-icons-png.flaticon.com/512/149/149071.png"
 html_text = f"""
 <div style="display:flex;align-items:center;gap:8px">
-< img src="{avatar_url}" width="36" height="36">
+<img src="{avatar_url}" width="36" height="36">
 <span style="font-size:14px;color:#666">简易计算器 | 制作者：Ms.Zhang</span>
 </div>
 """
